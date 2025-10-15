31e6ddae042af7b6b02e8866b4bf681aea07a9b7
Oalid Khan <oalidkhan69@gmail.com>
2025-10-15 01:48:13 -0700

    Create CapitalorSmallorDigit.c

---
+++ Semester 1/Module 05/CapitalorSmallorDigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    char X;
+    scanf(" %c",&X);
+    if(X>='0' && X<='9')
+    {
+        printf("IS DIGIT");
+    }
+    else if(X>='a' && X<='z')
+    {
+        printf("ALPHA\nIS SMALL");
+    }
+    else if(X>='A' && X<='Z')
+    {
+        printf("ALPHA\nIS CAPITAL");
+    }
+
+    return 0;
+}
