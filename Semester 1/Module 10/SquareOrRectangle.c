2f722e1c4874224162b45189ab82eb037939d274
Oalid Khan <oalidkhan69@gmail.com>
2025-11-13 22:33:49 +0600

    Create SquareOrRectangle.c

---
+++ Semester 1/Module 10/SquareOrRectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int t,w,h;
+    scanf("%d",&t);
+    for(int i=1 ; i<=t ; i++)
+    {
+        scanf("%d %d",&w,&h);
+        if(w==h)
+            printf("Square\n");
+        else
+            printf("Rectangle\n");
+    }
+
+    return 0;
+}
